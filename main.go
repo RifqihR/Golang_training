08f5a49e8ae226b002527c3fe68254f4e1b9a007
RifqiHanif <rifqihanif24@gmail.com>
2023-03-08 17:29:09 +0700

    finishing the challenge 1

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+)
+
+var i = 21
+var j = true
+var k float64 = 123.456
+
+func main() {
+	fmt.Println(i)
+	fmt.Printf("%T\n", i)
+	fmt.Println("%")
+	fmt.Println(j)
+
+	fmt.Printf("%b\n", i)
+	fmt.Printf("%o\n", i)
+	fmt.Printf("%x\n", i)
+
+	tmp := hex.EncodeToString([]byte("f"))
+	out, err := hex.DecodeString(tmp)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+
+	tmp = hex.EncodeToString([]byte("F"))
+	out, err = hex.DecodeString(tmp)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(string(out))
+
+	checking := "Я"
+	ck := []rune(checking)
+	fmt.Printf("%U\n", ck[0])
+	//for _, runeValue := range checking {
+	//	fmt.Printf("%T\n", runeValue)
+	//}
+	fmt.Printf("%f\n", k)
+	fmt.Printf("%E\n", k)
+}
